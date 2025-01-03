7b8b7bce59dd6f4f50eb6d4b7e21532f8bdafadd
Luis Carrasco <125158797+ilmovilDev@users.noreply.github.com>
2025-01-03 17:43:31 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { corsConfig } from './config/cors.config';
+
+const DEFAULT_PORT = 3001;  
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+  const port = Number(process.env.APP_PORT) ?? DEFAULT_PORT
+
+  app.enableCors(corsConfig());
+  app.setGlobalPrefix("api/v1.0");
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  
+  await app.listen(port);
+  logger.log(`App running on port ${ port }`);
 }
 bootstrap();
